8edf3828eded0ce4091c0b061dbf2b241aba1a3d
Ethan Kim <97044938+ByeByeKIMKIM@users.noreply.github.com>
2024-11-14 00:00:44 -0500

    Update firebase.ts

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -11,26 +11,3 @@ const db = getFirestore();
 
 
 export { db };
-
-//here
-
-// // Import the functions you need from the SDKs you need
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAB33nYzhorRK0CwsZ8-wnqSvcDTrcO-vE",
-//   authDomain: "trendsinwebdevfinalproject.firebaseapp.com",
-//   projectId: "trendsinwebdevfinalproject",
-//   storageBucket: "trendsinwebdevfinalproject.firebasestorage.app",
-//   messagingSenderId: "524099176427",
-//   appId: "1:524099176427:web:ffdc7a398b0572c620bbae",
-//   measurementId: "G-93J7DGZNY4"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
